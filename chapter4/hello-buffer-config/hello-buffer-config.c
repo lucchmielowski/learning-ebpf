9d2638e1d40d0c24fcec6666431fbc132ecf0836
Luc Chmielowski <luc.chmielowski@nirmata.com>
2025-08-17 17:19:35 +0200

    add chapter 4
    Signed-off-by: Luc Chmielowski <luc.chmielowski@nirmata.com>

---
+++ chapter4/hello-buffer-config/hello-buffer-config.c
@@ -0,0 +1,49 @@
+//go:build ignore
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+
+struct user_msg_t {
+	char message[12];
+};
+
+struct data_t {
+	int pid;
+	int uid;
+	char command[16];
+	char message[12];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1024);
+	__type(key, __u32);
+	__type(value, struct user_msg_t);
+} user_config SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} output SEC(".maps");
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int hello(struct trace_event_raw_sys_enter *ctx) {
+	char default_msg[12] = "Hello World";
+	struct data_t data = {};
+
+	data.pid = bpf_get_current_pid_tgid() >> 32;
+	data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+	bpf_get_current_comm(&data.command, sizeof(data.command));
+
+	struct user_msg_t *p = bpf_map_lookup_elem(&user_config, &data.uid);
+	if (p) {
+		__builtin_memcpy(data.message, p->message, sizeof(data.message));
+	} else {
+		__builtin_memcpy(data.message, default_msg, sizeof(data.message));
+	}
+
+	bpf_perf_event_output(ctx, &output, BPF_F_CURRENT_CPU, &data, sizeof(data));
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
